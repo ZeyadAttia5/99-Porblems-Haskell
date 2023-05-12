87d244f048d2ee3c937127bd55d209e6022f0ff2
Zeyad Mohamed Attia <85710632+ZeyadAttia5@users.noreply.github.com>
2023-05-12 23:14:14 +0300

    solved till isPalindrome from the lecture

--- Main.hs
+++ Main.hs
@@ -9,3 +9,20 @@ doubleAll (h:t) =  (h*2) : (doubleAll t)
 --- A function that returns the tail of a list
 tail [] = error "List cannot be empty"
 tail(h:t) = t
+
+
+--- A function that flattens a list of lists
+flatten [] = []
+flatten (a:t) = a ++ flatten t
+
+--- A function that returns all even elements of a list in another list
+getEven [] = []
+getEven (a:t) | even a = a : getEven t
+              | otherwise getEven t
+
+
+
+--- A function that summs all elements of a list
+-- isPalindrome [] = True
+-- isPalindrome [a] = True
+-- isPalindrome (a:t) = if a == (last t) then isPalindrome (init t) else False
